aa11831fbd97f115fbfe9b7b9dca86ce5edb6363
avrians <avriansyahb10@gmail.com>
2024-01-09 06:07:56 +0700

    [av][Access Modifier]

--- 44packageImport.go
+++ 44packageImport.go
@@ -6,4 +6,8 @@ import "fmt"
 func main()  {
 	result := helper.SayHello("Eko")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // tidak bisa diakses karena nama variabel diawali huruf kecil
+	// fmt.Println(helper.sayGoodBye("Eko") // tidak bisa diakses karena nama fungsi diawali huruf kecil
 }
--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,13 @@
 package helper
 
+var version = "1.0.0" // tidak bisa diakses dari luar package
+var Application = "Belajar Golang" // bisa diakses dari luar package
+// nama harus diawali dengan huruf besar agar bisa diakses dari luar package
+
 func SayHello(name string) string {
 	return "Hello " + name
+}
+
+func sayGoodBye(name string) string {
+	return "Good Bye " + name
 }
